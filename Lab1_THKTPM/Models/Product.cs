96c7591519928185be9f89c15e88e9b5de704f9c
DangXuanDuc204 <dangxuanduc29052004@gmail.com>
2025-03-05 20:06:44 +0700

    Update Lab2

---
+++ Lab1_THKTPM/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab1_THKTPM.Models
+{
+    namespace ASC.Model.Models
+    {
+        public class Product
+        {
+            public int ProductId { get; set; }
+            public string Name { get; set; }
+        }
+    }
+
+}
